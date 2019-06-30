ad6e0c8bcb33edfc9e831b315545e57379c509a9
Neiva07 <lucasneiva07@gmail.com>
2019-06-30 13:46:14 -0300

    added helper functions to structure all responses

--- controllers/authentication.controller.go
+++ controllers/authentication.controller.go
@@ -1,12 +1,34 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/jinzhu/gorm"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Token to represent a token of a user
+type Token struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+// Account struct to represent the account of a user
+type Account struct {
+	gorm.Model
+
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Token    string `json:"token";sql:"-"`
+}
+
+// Authenticate check and auth the user who is trying to signin
 var Authenticate = func(w http.ResponseWriter, r *http.Request) {
 
 }
 
-var SignUp = func(w http.Response Writer,  r*http.Request) {
+// SignUp create a user in the database
+var SignUp = func(w http.ResponseWriter, r *http.Request) {
 
-	
 }
