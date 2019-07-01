f1532b2ab966183e6f12bb0bc9b0349c49ae6292
Neiva07 <lucasneiva07@gmail.com>
2019-07-01 09:12:14 -0300

    JWT token finished

---
+++ app/auth.go
@@ -0,0 +1,80 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"pokemon-server/models"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+
+	u "pokemon-server/utils"
+)
+
+// JwtAuthentication handle with token management of all routes. It's a middleware that
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		notAuth := []string{"api/users/login", "api/users/signup"}
+
+		requestPath := r.URL.Path
+
+		for _, value := range notAuth {
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization")
+
+		if tokenHeader == "" {
+			response = u.Message(false, "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Response(w, response)
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ")
+
+		if len(splitted) != 2 {
+			response = u.Message(false, "Invalid/Malformed auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Response(w, response)
+			return
+		}
+
+		tokenPart := splitted[1]
+		tk := &models.Token{}
+
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil {
+			response = u.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Response(w, response)
+		}
+
+		if !token.Valid {
+			response = u.Message(false, "Token is not valid")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Response(w, response)
+		}
+
+		fmt.Printf("User %v", tk.UserId)
+		ctx := context.WithValue(r.Context(), "user", tk.UserId)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+
+	})
+}
