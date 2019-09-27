4e8fd217b227398d6c8ada523265db61bb2dea36
Matheus Pereira Araujo <matheuspereiraaraujo@MacBook-Pro-de-Regina.local>
2019-09-26 21:42:18 -0300

    Put Components inside Page dir

--- mobile_client/routes.js
+++ mobile_client/routes.js
@@ -1,4 +1,4 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import Login from './Login';
-import Main from './Main';
+import Login from './pages/Login';
+import Main from './pages/Main';
 export default createAppContainer(createSwitchNavigator({Login, Main}));
